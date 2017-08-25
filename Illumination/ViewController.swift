889020110761e97b7cdcf75698ce95b3cc9e203d
Neel Bhalla <ashdawngary@gmail.com>
2017-08-25 12:06:37 -0400

    Initial Commit

---
+++ Illumination/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Illumination
+//
+//  Created by Sarita Malhotra on 8/25/17.
+//  Copyright © 2017 Illumination DevTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
